833c4b4c95add0d7d301685ce21b22a7625b7935
TarsilaSamille <tarsillasamile@gmail.com>
2021-06-03 16:51:58 -0300

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -37,21 +37,7 @@ module.exports = {
             zIndex: ["hover", "active"],
         },
     },
-    plugins: [
-        'tailwindcss',
-        'postcss-flexbugs-fixes', [
-            'postcss-preset-env',
-            {
-                autoprefixer: {
-                    flexbox: 'no-2009',
-                },
-                stage: 3,
-                features: {
-                    'custom-properties': false,
-                },
-            },
-        ],
-    ],
+    plugins: [],
     future: {
         purgeLayersByDefault: true,
     },
